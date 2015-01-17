8a6df9aff881050c46d036858f8d4d22d2204b9d
Michael Pope <mpope.cr@gmail.com>
2015-01-17 16:25:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pope4president/webviewexample/HeyLookAWebView.java
@@ -0,0 +1,53 @@
+package com.example.pope4president.webviewexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class HeyLookAWebView extends Activity {
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hey_look_aweb_view);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.setWebViewClient(new MyWebViewClient());
+        String uri = "http://www.google.com";
+        webview.loadUrl(uri);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hey_look_aweb_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
